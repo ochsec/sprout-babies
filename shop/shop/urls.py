e7934a8e45e4dc5c7428a224c0e3ece703b26c7d
Chris Ochsenreither <cochsenreither@protonmail.com>
2020-10-09 14:37:59 -0700

    created cart application

--- shop/shop/urls.py
+++ shop/shop/urls.py
@@ -20,6 +20,7 @@ from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('cart/', include('cart.urls', namespace='cart')),
     path('', include('app.urls', namespace='app')),
 ]
 if settings.DEBUG:
