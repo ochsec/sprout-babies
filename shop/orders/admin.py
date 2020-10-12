96abb5f3a61877d104fd56c5e1aba1b93dbb266c
Chris Ochsenreither <cochsenreither@protonmail.com>
2020-10-12 10:31:25 -0700

    created invoice template

--- shop/orders/admin.py
+++ shop/orders/admin.py
@@ -33,6 +33,12 @@ def order_detail(obj):
     url = reverse('orders:admin_order_detail', args=[obj.id])
     return mark_safe(f'<a href="{url}">View</a>')
 
+def order_pdf(obj):
+    url = reverse('orders:admin_order_pdf', args=[obj.id])
+    return mark_safe(f'<a href="{url}">PDF</a>')
+
+order_pdf.short_description = 'Invoice'
+
 class OrderItemInline(admin.TabularInline):
     model = OrderItem
     raw_id_fields = ['product']
@@ -50,7 +56,8 @@ class OrderAdmin(admin.ModelAdmin):
         'paid', 
         'created', 
         'updated',
-        order_detail
+        order_detail,
+        order_pdf,
     ]
 
     list_filter = [
