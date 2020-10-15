a3249d093cc1747778b44226afb67ce13f7e493e
Chris Ochsenreither <cochsenreither@protonmail.com>
2020-10-14 19:12:21 -0700

    product search box

--- shop/app/views.py
+++ shop/app/views.py
@@ -1,23 +1,29 @@
+import json
 from django.shortcuts import render, get_object_or_404
 from .models import Category, Product
 from cart.forms import CartAddProductForm
 
 def home(request):
     categories = Category.objects.all()
+    products = Product.objects.filter(available=True)
+    product_search = [{'name': p.name, 'url': p.get_absolute_url()} for p in products]
     return render(request,
         'app/home.html',
-        {'categories': categories})
+        {'categories': categories,
+        'product_search': json.dumps(product_search)})
 
 def product_list(request, category_slug=None):
     category = None
     categories = Category.objects.all()
     products = Product.objects.filter(available=True)
+    product_search = [{'name': p.name, 'url': p.get_absolute_url()} for p in products]
     if category_slug:
         category = get_object_or_404(Category, slug=category_slug)
         products = products.filter(category=category)
     return render(request,
         'app/product/list.html',
         {'category': category,
+        'product_search': json.dumps(product_search),
         'categories': categories,
         'products': products})
 
@@ -27,8 +33,13 @@ def product_detail(request, id, slug):
         slug=slug,
         available=True)
     cart_product_form = CartAddProductForm()
+    categories = Category.objects.all()
+    products = Product.objects.filter(available=True)
+    product_search = [{'name': p.name, 'url': p.get_absolute_url()} for p in products]
     return render(request,
         'app/product/detail.html',
         {'product': product,
+        'categories': categories,
+        'product_search': json.dumps(product_search),
         'cart_product_form': cart_product_form})
 
