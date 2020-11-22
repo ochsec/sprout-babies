b149850e38d959f798d0f8b24293a2468fbf1b4e
Christopher Ochsenreither <cochsenreither@protonmail.com>
2020-11-22 20:35:53 +0000

    updated database info

--- shop/shop/settings/prod.py
+++ shop/shop/settings/prod.py
@@ -18,17 +18,27 @@ ALLOWED_HOSTS = ['*']
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, "db.sqlite3"),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'sprout-dev',
+        'USER': 'doadmin',
+        'PASSWORD': 'alt98m9u4wquaeyx',
+        'HOST': 'db-postgresql-sfo2-98569-do-user-1287700-0.b.db.ondigitalocean.com',
+        'PORT': 25060
     }
 }
 
-EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+# EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST_USER = 'sproutbabies2@gmail.com'
+EMAIL_HOST_PASSWORD = 'erEhG7b4QQ3j<H'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
 
 # Braintree settings
-BRAINTREE_MERCHANT_ID = os.getenv('DEV_BRAINTREE_MERCHANT_ID') # Merchant ID
-BRAINTREE_PUBLIC_KEY = os.getenv('DEV_BRAINTREE_PUBLIC_KEY') # Public key
-BRAINTREE_PRIVATE_KEY = os.getenv('DEV_BRAINTREE_PRIVATE_KEY') # Private key
+BRAINTREE_MERCHANT_ID = 'vmb2qqxsjqzk2z84' # Merchant ID
+BRAINTREE_PUBLIC_KEY = 'k44ztpp5sg5ygc2d' # Public key
+BRAINTREE_PRIVATE_KEY = '903b70d305baa7a93b7ddd118fa630d7' # Private key
 
 import braintree
 
